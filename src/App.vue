43f9c015128a5b52640ab40200f2b26243a8f24a
Garcia <dong.yy1916@gmail.com>
2024-05-05 14:46:04 +0800

    feat: heic2png;pension calculate

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+    <RouterView />
+</template>
+
+<style>
+:root {
+  --height: 200px;
+}
+
+.primary-btn {
+  /* 设置按钮的背景颜色和文本颜色 */
+  background-color: #00c853;
+  color: #fff;
+
+  /* 设置按钮的边框和圆角 */
+  border: none;
+  border-radius: 5px;
+
+  /* 设置按钮的内边距和外边距 */
+  padding: 10px 20px;
+  margin: 10px;
+
+  /* 设置按钮的文字样式 */
+  font-size: 16px;
+  font-weight: bold;
+  text-transform: uppercase;
+
+  /* 设置按钮的阴影效果 */
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.3), 0px 2px 4px rgba(0, 0, 0, 0.2);
+  transform: translateY(0);
+
+  /* 设置按钮的鼠标悬停样式 */
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.primary-btn:hover {
+  /* 鼠标悬停时改变按钮的位置和阴影效果 */
+  transform: translateY(-3px);
+  box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.4), 0px 4px 6px rgba(0, 0, 0, 0.3);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import './assets/windIndex.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+
+// 全局注册ElementPlusIconsVue组件
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
